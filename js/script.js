5e07925b160c9bee6d658457c7a77cdc15ddd413
michele-pupo <mikypupo@hotmail.it>
2024-01-30 16:31:25 +0100

    end of ticket calculation

--- js/script.js
+++ js/script.js
@@ -19,3 +19,50 @@
 // (il prezzo dovrà essere formattato con massimo due decimali, per indicare i centesimi sul prezzo).
 //  Questo richiederà un minimo di ricerca.
 
+// salvataggio degli elementi del DOM
+const buttonElement = document.querySelector("#price");
+const inputAgeElement = document.querySelector("#user-age");
+const inputKmElement = document.querySelector("#user-km");
+
+// prezzo al chilometro
+const priceForKm = 0.21;
+// sconti per fascia di età
+const percentageOfMinors = 20;
+const percentageOver65 = 40;
+
+
+// al click prende i valori di anni e km ed effettua il calcolo del prezzo del biglietto
+buttonElement.addEventListener('click',
+    function(){
+
+        // calcolo prezzo biglietto intero con doppia cifra dopo la virgola
+        const priceToTicket = ((priceForKm * inputKmElement.value).toFixed(2));
+
+        // calcolo sconto minorenni
+        const discountForMinors = (((priceToTicket / 100) * percentageOfMinors));
+        // calcolo sconto over 65
+        const discountForOver65 = (((priceToTicket / 100) * percentageOver65));
+        
+
+        // utente minorenne sconto 20%
+        if (inputAgeElement.value < 18){
+            // sconto del 20%
+            console.log("-20%");
+            // calcolo prezzo del biglietto
+            document.querySelector("#ticket-price").innerHTML = `${(priceToTicket - discountForMinors).toFixed(2)} €`;
+
+            // utente con età compresa tra 18 e 65 niente sconto
+        } else if (inputAgeElement.value >= 18 && inputAgeElement.value < 65){
+            console.log("prezzo intero");
+            // calcolo prezzo del biglietto
+            document.querySelector("#ticket-price").innerHTML = `${(priceToTicket)} €`;
+
+            // utente over 65 sconto 40%
+        } else {
+            // sconto del 40%
+            console.log("-40%");
+            // calcolo prezzo del biglietto
+            document.querySelector("#ticket-price").innerHTML = `${(priceToTicket - discountForOver65).toFixed(2)} €`;
+        }
+    }
+);
